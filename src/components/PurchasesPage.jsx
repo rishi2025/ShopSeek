da4fc64ddc3c0b2dd0f4b7eee7b31f18065b7517
klynweto <snehanau85@gmail.com>
2024-08-12 13:02:48 +0530

    Purchases Page component created.

---
+++ src/components/PurchasesPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {PurchasedCard, Container} from './mainComponent';
+
+function PurchasesPage() {
+  return (
+    <Container className='flex flex-row flex-wrap gap-y-8 my-8 h-[45.5rem] overflow-y-scroll    '>
+        <PurchasedCard 
+        title="NIKE SHOES"
+        buyerName="Shreyash Singh"
+        buyerImage="https://i.ibb.co/sCnF7Gk/image-44.png"
+        sellerName="Shreyash Singh"
+        sellerImage="https://i.ibb.co/sCnF7Gk/image-44.png"
+        sellerProductImage={["https://i.ibb.co/gt7gzBn/image-42.png"]}
+        buyerProductImage={["https://i.ibb.co/gt7gzBn/image-42.png"]}
+        price={1200}
+        orderDate="26-07-2024"
+        size="14"
+        description="White and pink color shoes or sneakers to throw on people around me because I gud."
+        tags={["Shoes", "Nike", "Sneakers", "Pink"]}
+      />
+    </Container>
+
+  )
+}
+
+export default PurchasesPage
