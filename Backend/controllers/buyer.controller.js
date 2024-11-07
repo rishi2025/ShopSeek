f7dc10c11a1aa170e727892e2ecae7707d45d527
yashikaCool <yashika110204@gmail.com>
2024-11-07 17:28:27 +0530

    register buyer  api added

--- Backend/controllers/buyer.controller.js
+++ Backend/controllers/buyer.controller.js
@@ -19,3 +19,52 @@ const generateAccessAndRefreshTokens = async (buyerId) => {
         throw new ApiError(500, "Something went wrong while generating Access and Refresh tokens...");
     }
 };
+
+const registerBuyer = asyncHandler(async (req, res) => {
+
+    // get Buyer details from frontend
+    const { email, name, password } = req.body;
+
+
+    // validation - not empty
+    if (
+        [email, name, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required...");
+    }
+
+    // check if buyer already exists: email
+    const existingEmail = await Buyer.findOne({
+        email
+    });
+
+    if (existingEmail)
+        throw new ApiError(409, `email is already registered.`);
+
+    // create buyer object - create entry in db
+    const buyer = await Buyer.create({
+        email,
+        name,
+        password,
+    });
+
+    // remove hashed password and refresh token field from response
+    const createdBuyer = await Buyer.findById(buyer._id).select(
+        "-password -refreshTokens"
+    );
+
+
+    // check for buyer creation
+    if (!createdBuyer)
+        throw new ApiError(500, "Something went wrong while registering the buyer...");
+
+
+    // return response
+    return res.status(201).json(
+        new ApiResponse(200, createdSeller, "buyer registered successfully !!!")
+    )
+});
+
+export {
+    registerBuyer
+};
